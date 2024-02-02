9b13608f8015b11b8e4c38b8832a3e102bcb2838
AdemTag <137310901+AdemTag@users.noreply.github.com>
2024-02-02 05:05:33 +0100

    first commit

---
+++ src/app/detail-pdoc/detail-pdoc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-pdoc',
+  templateUrl: './detail-pdoc.component.html',
+  styleUrls: ['./detail-pdoc.component.css']
+})
+export class DetailPdocComponent {
+
+}
